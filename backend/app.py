47a3471abbd531ac726556f94f38514cfd6c0ee8
AshAnand34 <aashishanand2019@gmail.com>
2025-05-25 14:06:47 -0700

    Ensure NLTK and TextBlob corpora are downloaded during app startup for improved text processing

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,7 @@ import logging
 from time import sleep
 import nltk
 import base64
+from textblob import download_corpora
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["https://kpop-moodify.netlify.app", "http://localhost:3000"], "methods": ["GET", "POST", "OPTIONS"]}}, supports_credentials=True)
@@ -230,13 +231,16 @@ def health_check():
 
 if __name__ == '__main__':
     # Ensure required NLTK data is downloaded during startup
-    nltk_data_packages = ["punkt", "averaged_perceptron_tagger", "wordnet"]
+    nltk_data_packages = ["punkt", "punkt_tab" "averaged_perceptron_tagger", "wordnet"]
     for package in nltk_data_packages:
         try:
             nltk.download(package)
         except Exception as e:
             print(f"Error downloading NLTK package {package}: {e}")
     
+    # Ensure required TextBlob corpora are downloaded during startup
+    download_corpora()
+
     # Start the Flask app
     port = int(os.environ.get('PORT', 5000))
     app.run(host="0.0.0.0", port=port)
